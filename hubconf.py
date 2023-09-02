0c814e4d2a2c984570ece3d45c96492546297eff
nicolvisser <nicolvisser@yahoo.com>
2023-09-03 01:35:15 +0200

    update hubconf

--- hubconf.py
+++ hubconf.py
@@ -14,11 +14,13 @@ import torch
 from kmeans import KMeansInference
 
 
-def _kmeans(
+def kmeans(
     n_clusters: int,
     pretrained: bool = True,
     progress: bool = True,
 ) -> KMeansInference:
+    assert n_clusters in URLS.keys(), f"n_clusters must be one of {URLS.keys()}"
+
     model = KMeansInference(k=n_clusters, d=768)
     if pretrained:
         checkpoint = torch.hub.load_state_dict_from_url(
@@ -27,81 +29,3 @@ def _kmeans(
         model.cluster_centers.data = torch.from_numpy(checkpoint["cluster_centers_"])
         model.eval()
     return model
-
-
-def kmeans_50(
-    pretrained: bool = True,
-    progress: bool = True,
-) -> KMeansInference:
-    """
-    KMeans model with 50 clusters trained on the HuBERT embeddings of the LibriSpeech train-clean-100 dataset.
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on LibriSpeech train-clean-100
-        progress (bool): If True, displays a progress bar of the download to stderr
-    """
-    return _kmeans(50, pretrained, progress)
-
-
-def kmeans_100(
-    pretrained: bool = True,
-    progress: bool = True,
-) -> KMeansInference:
-    """
-    KMeans model with 100 clusters trained on the HuBERT embeddings of the LibriSpeech train-clean-100 dataset.
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on LibriSpeech train-clean-100
-        progress (bool): If True, displays a progress bar of the download to stderr
-    """
-    return _kmeans(100, pretrained, progress)
-
-
-def kmeans_200(
-    pretrained: bool = True,
-    progress: bool = True,
-) -> KMeansInference:
-    """
-    KMeans model with 200 clusters trained on the HuBERT embeddings of the LibriSpeech train-clean-100 dataset.
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on LibriSpeech train-clean-100
-        progress (bool): If True, displays a progress bar of the download to stderr
-    """
-    return _kmeans(200, pretrained, progress)
-
-
-def kmeans_500(
-    pretrained: bool = True,
-    progress: bool = True,
-) -> KMeansInference:
-    """
-    KMeans model with 500 clusters trained on the HuBERT embeddings of the LibriSpeech train-clean-100 dataset.
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on LibriSpeech train-clean-100
-        progress (bool): If True, displays a progress bar of the download to stderr
-    """
-    return _kmeans(500, pretrained, progress)
-
-
-def kmeans_1000(
-    pretrained: bool = True,
-    progress: bool = True,
-) -> KMeansInference:
-    """
-    KMeans model with 1000 clusters trained on the HuBERT embeddings of the LibriSpeech train-clean-100 dataset.
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on LibriSpeech train-clean-100
-        progress (bool): If True, displays a progress bar of the download to stderr
-    """
-    return _kmeans(1000, pretrained, progress)
-
-
-def kmeans_2000(
-    pretrained: bool = True,
-    progress: bool = True,
-) -> KMeansInference:
-    """
-    KMeans model with 2000 clusters trained on the HuBERT embeddings of the LibriSpeech train-clean-100 dataset.
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on LibriSpeech train-clean-100
-        progress (bool): If True, displays a progress bar of the download to stderr
-    """
-    return _kmeans(2000, pretrained, progress)
