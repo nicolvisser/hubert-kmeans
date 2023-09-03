9de63673d4d2d04c8cea4208d3b09a5c065140b2
nicolvisser <nicolvisser@yahoo.com>
2023-09-03 18:03:49 +0200

    add hashes to hubconf links

--- hubconf.py
+++ hubconf.py
@@ -1,12 +1,12 @@
 dependencies = ["torch"]
 
 URLS = {
-    50: "https://github.com/nicolvisser/hubert-kmeans/releases/download/v0.1/kmeans-50.pt",
-    100: "https://github.com/nicolvisser/hubert-kmeans/releases/download/v0.1/kmeans-100.pt",
-    200: "https://github.com/nicolvisser/hubert-kmeans/releases/download/v0.1/kmeans-200.pt",
-    500: "https://github.com/nicolvisser/hubert-kmeans/releases/download/v0.1/kmeans-500.pt",
-    1000: "https://github.com/nicolvisser/hubert-kmeans/releases/download/v0.1/kmeans-1000.pt",
-    2000: "https://github.com/nicolvisser/hubert-kmeans/releases/download/v0.1/kmeans-2000.pt",
+    50: "https://github.com/nicolvisser/hubert-kmeans/releases/download/v0.1/kmeans-50-136557c6.pt",
+    100: "https://github.com/nicolvisser/hubert-kmeans/releases/download/v0.1/kmeans-100-2b630172.pt",
+    200: "https://github.com/nicolvisser/hubert-kmeans/releases/download/v0.1/kmeans-200-2b23038c.pt",
+    500: "https://github.com/nicolvisser/hubert-kmeans/releases/download/v0.1/kmeans-500-0957efb2.pt",
+    1000: "https://github.com/nicolvisser/hubert-kmeans/releases/download/v0.1/kmeans-1000-c75946f8.pt",
+    2000: "https://github.com/nicolvisser/hubert-kmeans/releases/download/v0.1/kmeans-2000-d71187e5.pt",
 }
 
 import torch
